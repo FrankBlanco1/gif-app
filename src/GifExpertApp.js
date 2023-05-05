3dbf25f6ab7b5bdd88cf1637eea9a9e4bedd2288
FrankBlanco1 <frankbg.000914@gmail.com>
2023-05-05 13:51:46 -0400

    Create a array of categories, list this categories and create a button to add a new predefined category

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,36 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 
 export const GifExpertApp = () => {
+
+    // const categories = ['One Piece', 'Dark Souls'];
+    const [categories, setCategories] = useState(['One Piece', 'Dark Souls']);
+
+    const handleAdd = () => {
+
+        setCategories((categories) => {
+            return [...categories, 'HunterXHunter']
+        });
+
+    }
+
+    console.log(categories)
 
     return <Fragment>
         <h2>GifExpertApp</h2>
         <hr />
+
+        <button onClick={handleAdd}>Add</button>
+
+        <ol>
+            {
+                // map retorna dos objetos: el elemento y su indice
+                // los key deben ser ids unicos, generalmente id de elementos de bases de datos
+                categories.map((category, i) => {
+                    return <li key={category}>{category}</li>;
+                })
+            }
+        </ol>
+
     </Fragment>
 
 }
