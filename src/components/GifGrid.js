72beb2b9930b131860d2c2160a1a4f41be211ef9
FrankBlanco1 <frankbg.000914@gmail.com>
2023-05-07 11:20:33 -0400

    Introduction to the custom hooks, in this case we create a hook to wait3 seconds for show data, like a loading state

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,30 +1,33 @@
 
-import React, { Fragment, useState, useEffect } from 'react'
-import { GifGridItem } from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+//import { GifGridItem } from './GifGridItem';
+//import { getGifs } from '../helpers/getGifs';
 
 // api_key = lzkj9sxJUc5oxbFw4vlB31jMUsd1Z0AV
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
+    const {loading} = useFetchGifs();
 
-    // use Effect recibe dos parametros:
-    // -El primero es una funcion a ejecutarde estas dependecias depende que se vuelva a ejecutar la primera funcion
-    // -El segundo una lista de dependencias, 
-    // En este caso cunado el segundo argumento es [], se ejecuta la funcion una sola vez al inicio
-    useEffect( () => {
+    // // use Effect recibe dos parametros:
+    // // -El primero es una funcion a ejecutarde estas dependecias depende que se vuelva a ejecutar la primera funcion
+    // // -El segundo una lista de dependencias, 
+    // // En este caso cunado el segundo argumento es [], se ejecuta la funcion una sola vez al inicio
+    // useEffect( () => {
 
-        getGifs(category).then( (images) => {
-            setImages(images);
-        });
+    //     getGifs(category).then( (images) => {
+    //         setImages(images);
+    //     });
 
-    }, [category]);
+    // }, [category]);
 
     return (
         <>
             <h3>{category}</h3>
-            <div className='card-grid'> 
+            {loading? 'Loading...' : 'Complete load'}
+            {/* <div className='card-grid'> 
                 <ol>
                     {
                         images.map( (img, i) => {
@@ -37,7 +40,7 @@ export const GifGrid = ({category}) => {
                         }) 
                     }
                 </ol>
-            </div>
+            </div> */}
         </>
     )
 }
