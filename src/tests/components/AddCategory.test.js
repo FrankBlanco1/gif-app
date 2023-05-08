6592a122f45da069a6fd825ad50ef8547092b2ee
FrankBlanco1 <frankbg.000914@gmail.com>
2023-05-08 15:18:53 -0400

    Add other test to AddCategory component, in this case we test the change in the input text

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,4 +1,4 @@
-import {render, screen } from "@testing-library/react";
+import {render, screen, fireEvent } from "@testing-library/react";
 import { AddCategory } from "../../components/addCategory";
 
 describe("Testing the AddCategory component", () => {
@@ -15,6 +15,18 @@ describe("Testing the AddCategory component", () => {
         // Default value of the input text
         expect(input.value).toBe('');
         expect(input.type).toBe('text');
+
+    });
+
+    test('Should change the text box value', () => {
+
+        const setCategories = () => {};
+
+        render(<AddCategory setCategories={setCategories}/>)
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, {target : {value : "Hello world"}});
+
+        expect(input.value).toBe('Hello world');
 
     });
 
