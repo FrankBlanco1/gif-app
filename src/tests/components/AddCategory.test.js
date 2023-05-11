f9bd30847d7cf90810e2129d6ca1f50295daf983
FrankBlanco1 <frankbg.000914@gmail.com>
2023-05-10 22:52:14 -0400

    Create a test for the input submit when the input value is empty

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -21,12 +21,32 @@ describe("Testing the AddCategory component", () => {
     test('Should change the text box value', () => {
 
         const setCategories = () => {};
+        const new_text = "Hello World";
 
         render(<AddCategory setCategories={setCategories}/>)
         const input = screen.getByRole('textbox');
-        fireEvent.change(input, {target : {value : "Hello world"}});
+        fireEvent.change(input, {target : {value : new_text}});
 
-        expect(input.value).toBe('Hello world');
+        expect(input.value).toBe(new_text);
+
+    });
+
+    // Con respecto al video del tutorial:
+    // expect(setCategories).not.toHaveBeenCalled() en este expect
+    // el setCategory se llama una vez porque el hace los cambios sobre el mismo wrapper (render)
+    // Nosotros hacemos render en cada prueba (porque si hacemos uno global nos da palo :( 
+
+    test("Should not post the information on submit 'couse inputValue is empty", () => {
+
+        const setCategories = jest.fn();
+        render(<AddCategory setCategories={setCategories}/>)
+
+        const input = screen.getByRole('textbox');
+        fireEvent.submit(input);
+
+        // Para saber si se llamo la funcion setCategories
+        expect(setCategories).not.toHaveBeenCalled();
+        expect(input.value).toBe('');
 
     });
 
